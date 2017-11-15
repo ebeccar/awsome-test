a327acb1ec10de09f50255ae79608d5c79576dfe
ebeccar <jag-rebecca@hotmail.com>
2017-11-15 16:48:15 +0100

    Add menu, changes text

--- index.php
+++ index.php
@@ -3,7 +3,20 @@
 		<title>Awesome test!</title>
 	</head>
 	<body>
-		<h1>This is a modest attempt to using Git.</h1>
-		<h2>Awesome!</h2>
+		<nav class="menu">
+			<ul>
+				<li><a href="">Lorem</a></li>
+				<li><a href="">Ipsum</a></li>
+				<li><a href="">Dolor</a></li>
+			</ul>
+		</nav>
+		<header>
+		<h1>Wow, that went really great!</h1>
+		<h2>Lorem ipsum dolor sit amet.</h2>
+		</header>
+		<section class="main">
+			<h2>New section, who dis?</h2>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</section>
 	</body>
 </html>
